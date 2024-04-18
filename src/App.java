d06994344e48375dc7deb5768b61931614f86592
Simon Nestler <simon.nestler@thi.de>
2024-04-18 11:12:01 +0200

    primitives einlesen der zeichen funktioniert.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        zeichenEinlesen();
+    }
+
+    public static void zeichenEinlesenMitGui() {
+
+    }
+
+    public static void zeichenEinlesen() throws IOException {
+        int i = 0;
+
+        while (i != 10) {
+            i = System.in.read();
+            System.out.print((char) i);
+        }
     }
 }
