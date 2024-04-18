d2031147a3deda9a99493c64d6cbcd6765cad56e
Simon Nestler <simon.nestler@thi.de>
2024-04-18 11:52:07 +0200

    Lesen der Datei abcde.txt

--- src/App.java
+++ src/App.java
@@ -1,18 +1,42 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        zeichenEinlesenMitBufferedReader();
+        // zeichenEinlesenMitBufferedReader();
+        // beispielePrintWriter();
+        dateiEinlesen();
+    }
+
+    public static void dateiEinlesen() {
+        try {
+            FileReader fr = new FileReader("abcde.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String zeile = br.readLine();
+            System.out.println(zeile);
+
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void zeichenEinlesenMitBufferedReader() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Bitte geben Ihren Vornamen ein: ");
-
         try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+
+            BufferedReader br = new BufferedReader(isr);
+            System.out.println("Bitte geben Ihren Vornamen ein: ");
+
             String vorname = br.readLine();
             System.out.println("Bitte geben Ihren Nachnamen ein: ");
             String nachname = br.readLine();
@@ -22,10 +46,24 @@ public class App {
             System.out.println("Ihr Name ist: " + vorname + " " + nachname +
                     " und Sie sind " + alter + " Jahre alt.");
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
+    }
+
+    public static void beispielePrintWriter() {
+        PrintWriter out;
+        try {
+            out = new PrintWriter("abcde.txt");
+
+            out.println("Hallo");
+            double price = 33.32499;
+            out.printf("Das Buch kostet %.2f Euro\n", price);
+            out.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Datei nicht gefunden");
+            e.printStackTrace();
+        }
     }
 
     public static void zeichenEinlesenMitGui() {
