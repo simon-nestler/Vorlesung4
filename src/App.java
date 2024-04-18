d30396f8e1bdd8b35179b401356d0d17f1783b57
Simon Nestler <simon.nestler@thi.de>
2024-04-18 11:14:50 +0200

    Text einlesen mit GUI

--- src/App.java
+++ src/App.java
@@ -1,11 +1,14 @@
 import java.io.IOException;
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        zeichenEinlesen();
+        zeichenEinlesenMitGui();
     }
 
     public static void zeichenEinlesenMitGui() {
+        String s = JOptionPane.showInputDialog("Bitte geben Sie einen Text ein: ");
+        System.out.println(s);
 
     }
 
