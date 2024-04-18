2e6437a88ec27775f94a273eb6a8a0f6f22da5d2
Simon Nestler <simon.nestler@thi.de>
2024-04-18 11:44:24 +0200

    zeichen einlesen mit buffered reader funktioniert

--- src/App.java
+++ src/App.java
@@ -1,15 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        zeichenEinlesenMitGui();
+        zeichenEinlesenMitBufferedReader();
+    }
+
+    public static void zeichenEinlesenMitBufferedReader() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Bitte geben Ihren Vornamen ein: ");
+
+        try {
+            String vorname = br.readLine();
+            System.out.println("Bitte geben Ihren Nachnamen ein: ");
+            String nachname = br.readLine();
+            System.out.println("Bitte geben Sie Ihr Alter ein: ");
+            int alter = Integer.parseInt(br.readLine());
+
+            System.out.println("Ihr Name ist: " + vorname + " " + nachname +
+                    " und Sie sind " + alter + " Jahre alt.");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
     public static void zeichenEinlesenMitGui() {
         String s = JOptionPane.showInputDialog("Bitte geben Sie einen Text ein: ");
         System.out.println(s);
-
     }
 
     public static void zeichenEinlesen() throws IOException {
